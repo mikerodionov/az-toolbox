0159b6a7e09c4a858cd40d52ed2546c44e1d35fc
Mikhail Rodionov <13007691+mikerodionov@users.noreply.github.com>
2026-03-20 11:28:44 +0100

    Update arcMachineLookup.sh

--- AzureAutomation/AzureGraphQueries/arcMachineLookup.sh
+++ AzureAutomation/AzureGraphQueries/arcMachineLookup.sh
@@ -1,7 +1,18 @@
+# Lookup for Arc machines by name to get RG, location, status and AAPLS information
+# The properties.privateLinkScopeResourceId on microsoft.hybridcompute/machines is always the Arc Private Link Scope (Microsoft.HybridCompute/privateLinkScopes). This controls the Arc agent (himds) connectivity.
 az graph query -q "Resources 
 | where type == 'microsoft.hybridcompute/machines' 
 | where name in~ ('MACHINE-A','MACHINE-B','MACHINE-C') 
-| project name, resourceGroup, subscriptionId, location, status=tostring(properties.status)" \
+| project name, resourceGroup, subscriptionId, location, status=tostring(properties.status), privateLinkScope=tostring(properties.privateLinkScopeResourceId)" \
   --first 10 \
-  --query "data[].{Name:name, ResourceGroup:resourceGroup, Subscription:subscriptionId, Location:location, Status:status}" \
+  --query "data[].{Name:name, ResourceGroup:resourceGroup, Location:location, Status:status, PrivateLinkScope:privateLinkScope}" \
+  -o table
+
+# The same but splitting PLS resource ID so that we get only PLS name
+az graph query -q "Resources 
+| where type == 'microsoft.hybridcompute/machines' 
+| where name in~ ('MACHINE-A','MACHINE-B','MACHINE-C') 
+| project name, resourceGroup, location, status=tostring(properties.status), plsName=tostring(split(properties.privateLinkScopeResourceId,'/')[8])" \
+  --first 10 \
+  --query "data[].{Name:name, ResourceGroup:resourceGroup, Location:location, Status:status, PrivateLinkScope:plsName}" \
   -o table
